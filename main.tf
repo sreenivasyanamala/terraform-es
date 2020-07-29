362513d623d14efbeb4744c0dd8862c8f44dae28
sreenivasyanamala <33051135+sreenivasyanamala@users.noreply.github.com>
2020-07-29 16:29:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ module "aws_es" {
   domain_name           = var.domain_name
   elasticsearch_version = var.elasticsearch_version
 
-  cluster_config = {
-    dedicated_master_enabled = "true"
-    dedicated_master_type    = var.dedicated_master_type
-    dedicated_master_count   = var.dedicated_master_count
-    instance_count           = var.instance_count
-    instance_type            = var.instance_type
-    zone_awareness_enabled   = "true"
-    availability_zone_count  = "3"
-  }
+ 
+    cluster_config_dedicated_master_enabled = true
+    cluster_config_dedicated_master_type    = var.dedicated_master_type
+    cluster_config_dedicated_master_count   = var.dedicated_master_count
+    cluster_config_instance_count           = var.instance_count
+    cluster_config_instance_type            = var.instance_type
+    cluster_config_zone_awareness_enabled   = "true"
+    cluster_config_availability_zone_count  = "3"
+  
 
   ebs_options = {
     ebs_enabled = "true"
