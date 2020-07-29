c0cda5388035161e4fe877b69f687e0f7be93690
sreenivasyanamala <33051135+sreenivasyanamala@users.noreply.github.com>
2020-07-29 15:06:00 +0530

    Update iam.tf

--- elastic-search/iam.tf
+++ elastic-search/iam.tf
@@ -28,7 +28,7 @@ CONFIG
 }
 
 # Service-linked role to give Amazon ES permissions to access your VPC
-resource "aws_iam_service_linked_role" "es" {
+resource "aws_iam_service_linked_role" "access-es" {
   count            = var.create_service_link_role == true ? 1 : 0
   aws_service_name = "es.amazonaws.com"
   description      = "Service-linked role to give Amazon ES permissions to access your VPC"
